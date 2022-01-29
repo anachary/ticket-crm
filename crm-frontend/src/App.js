2bcbdd589ed4d397db85024546e3ea8d7d9454e7
Akash Acharya <akashnacharya@gmail.com>
2022-01-29 18:10:07 -0500

    React App Frontend Setup.

---
+++ crm-frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {Button} from "react-bootstrap"
+function App() {
+  return (
+    <div className="App">
+      <Button>Test Button</Button>
+    </div>
+  );
+}
+
+export default App;
