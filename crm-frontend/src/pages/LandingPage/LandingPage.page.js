699bbd1f2b697331a8b9147203d044b9d8491faa
Akash Acharya <akashnacharya@gmail.com>
2022-02-06 13:35:35 -0500

    ui for login page

---
+++ crm-frontend/src/pages/LandingPage/LandingPage.page.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import {Login} from '../../components/login/Login.comp'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import "./landingPage.style.css"
+
+export const LandingPage = () => {
+  return <div className='root'>
+    <div className='dashboard-header'>
+      <div className='dashboard-content'>
+        <div><FontAwesomeIcon icon={faStar}/></div>
+        <div>Ticket CRM SYSTEM</div>
+      </div>
+    </div>
+    <div className='login-form-div'>
+      <div className='landing-page jumbotron form-box'>
+        <Login></Login>
+      </div>
+    </div>
+  </div>;
+};
