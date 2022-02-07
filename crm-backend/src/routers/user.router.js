3f780dcbbda997266194abbfd7a136c036040ade
Akash Acharya <akashnacharya@gmail.com>
2022-02-06 22:36:48 -0500

    TCCP-3-login-fr-01 endpoint added to compare hashpassword in the mongodb

--- crm-backend/src/routers/user.router.js
+++ crm-backend/src/routers/user.router.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
-const {insertUser} = require("../model/user/User.model")
-const {hashPassword} = require("../helpers/bcrypthelper")
+const {insertUser, getUserByEmail} = require("../model/user/User.model")
+const {hashPassword,comparePassword} = require("../helpers/bcrypthelper")
+const UserSchema = require("../model/user/User.schema")
     
 
 
@@ -34,5 +35,34 @@ router.post("/", async (req,res)=>{
         res.json({status:"error", message:error.message})
     }
 })
+
+
+router.post("/login", async (req,res)=>{
+    const {email, password} = req.body
+    try { 
+        if(!email || !password){
+            res.json({message:"Email and password cannot be empty.Please check the input or reset credentials.", status: "error"})
+        }
+
+        const user = await getUserByEmail(email);
+        console.log(user)
+        const hashPwd = user && user._id? user.password: null
+       
+        if (!hashPwd){
+            res.json({message:"Invalid email No user exists with this email. Please sign up", status: "error"})
+        }
+        const passwordMatch = await comparePassword(password, hashPwd)
+        if(!passwordMatch){
+            res.json({message:"Invalid combination of email and password. Please sign up or reset password", status: "error"})
+        }
+    
+        res.json({message:"Succesfully Login", status:"success"})
+        
+    } catch (error) {
+        console.log(error)
+        res.json({status:"error", message:error.message})
+    }
+})
+
 
 module.exports = router 
