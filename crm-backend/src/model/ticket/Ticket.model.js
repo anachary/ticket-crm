6a8dc49a046234b2a26abddda88a1f9bd94cd8f3
Akash Acharya <akashnacharya@gmail.com>
2022-03-20 19:43:06 -0400

    added ticket schema

---
+++ crm-backend/src/model/ticket/Ticket.model.js
@@ -0,0 +1,30 @@
+const { TicketSchema } = require("./Ticket.schema");
+
+const getTickets = (clientId) => {
+  return new Promise((resolve, reject) => {
+    try {
+      TicketSchema.find({ clientId })
+        .then((data) => resolve(data))
+        .catch((error) => reject(error));
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const getTicketById = (_id, clientId) => {
+  return new Promise((resolve, reject) => {
+    try {
+      TicketSchema.find({ _id, clientId })
+        .then((data) => resolve(data))
+        .catch((error) => reject(error));
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+module.exports = {
+  getTickets,
+  getTicketById
+};
