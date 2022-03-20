81523416e6524b01470cac30dab3d2f5009ffc6c
Akash Acharya <akashnacharya@gmail.com>
2022-03-20 19:48:50 -0400

    added ticket get all, get endpoints

---
+++ crm-backend/src/model/ticket/Ticket.schema.js
@@ -0,0 +1,91 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TicketSchema = new Schema({
+  clientId: {
+    type: Schema.Types.ObjectId,
+  },
+
+  subject: {
+    type: String,
+    maxlength: 100,
+    required: true,
+    default: "",
+  },
+
+  updatedDate: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+
+  updatedBy: {
+    type: String,
+    required:true,
+    default:""
+  },
+
+  priority: {
+    type:String,
+    required:"true",
+    default:"client"
+  },
+
+  issueDate:{
+    type: Date,
+    required: true,
+    default: Date.now()
+  },
+
+  assignedTo: {
+    type:String,
+    maxlength: 30,
+    default:null
+  },
+
+  assignedDate: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+
+  status: {
+    type: String,
+    maxlength: 30,
+    required: true,
+    default: "UnAssigned",
+  },
+
+  description: {
+    type:String,
+    maxlength: 1000,
+    required: true,
+    default: ""
+  },
+
+  conversations: [
+    {
+      sender: {
+        type: String,
+        maxlength: 50,
+        required: true,
+        default: "",
+      },
+      message: {
+        type: String,
+        maxlength: 1000,
+        required: true,
+        default: "",
+      },
+      msgAt: {
+        type: Date,
+        required: true,
+        default: Date.now(),
+      },
+    },
+  ],
+});
+
+module.exports = {
+  TicketSchema: mongoose.model("Ticket", TicketSchema),
+};
