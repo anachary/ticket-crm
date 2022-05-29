7950b3a20f7b709dfab3b6a1e3f84048f1d16cb7
Akash Acharya <akashnacharya@gmail.com>
2022-05-29 17:40:09 -0400

    Add  stub tests for the company Api

--- crm-frontend/src/api/companyApi.js
+++ crm-frontend/src/api/companyApi.js
@@ -4,71 +4,63 @@ const rootUrl = `http://${process.env.REACT_APP_BACKEND_SERVER_IP}:${process.env
 const companyUrl = rootUrl + "company/";
 const updateCompanyUrl = rootUrl + "company/update-company/";
 
-export const getAllCompanies = () => {
-  return new Promise(async (resolve, reject) => {
+export async function getAllCompanies() {
     try {
       const result = await axios.get(companyUrl, {
         headers: {
-          Authorization: sessionStorage.getItem("accessJWT"),
+          Authorization: sessionStorage.getItem("accessJWT")
         },
       });
-
-      resolve(result);
+      return result 
     } catch (error) {
-      reject(error);
+      throw error
     }
-  });
-};
+  }
 
-export const getSingleCompany = (_id) => {
-  return new Promise(async (resolve, reject) => {
+export async function getSingleCompany(_id){
     try {
       const result = await axios.get(companyUrl + _id, {
         headers: {
           Authorization: sessionStorage.getItem("accessJWT"),
         },
       });
-
-      resolve(result);
+      return result
     } catch (error) {
       console.log(error.message);
-      reject(error);
+     throw error
     }
-  });
 };
 
-
-export const updateCompany = (_id, companyObj) => {
-  return axios({
-    method: 'post',
-    url: updateCompanyUrl+_id,
-    headers: {
-      'Content-Type': ' application/json',
-      'Authorization': sessionStorage.getItem("accessJWT")
-    },
-    data:{...companyObj}
-  }).then((response) => {
-    console.log(response)
-  }).catch(error =>
-    { 
-      console.log(error)
-    })
+export  async function updateCompany(_id, companyObj) {
+  try {
+      const result = await axios({
+      method: 'post',
+      url: updateCompanyUrl+_id,
+      headers: {
+        'Content-Type': ' application/json',
+        'Authorization': sessionStorage.getItem("accessJWT")
+      },
+      data:{...companyObj}
+    })  
+    return result
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+ 
 };
 
-export const createNewCompany = (frmData) => {
+export async function createNewCompany(frmData){
   console.log("from api", frmData);
-  return new Promise(async (resolve, reject) => {
     try {
       const result = await axios.post(companyUrl, frmData, {
         headers: {
           Authorization: sessionStorage.getItem("accessJWT"),
         },
       });
-
-      resolve(result.data);
+      return result.data
     } catch (error) {
       console.log(error.message);
-      reject(error);
+      throw error
     }
-  });
 };
